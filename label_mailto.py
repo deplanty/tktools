27a545c602ca8b1290a7ac5f27e294cb8200d8b2
deplanty <38189611+deplanty@users.noreply.github.com>
2021-01-05 13:49:24 +0100

    Add type hints

--- label_mailto.py
+++ label_mailto.py
@@ -5,19 +5,19 @@ import webbrowser
 
 
 class LabelMailto(ttk.Label):
-    def __init__(self, master, text, to, subject, msg, style=None):
+    def __init__(self, master, text:str, to:list, subject:str, msg:str, style:str=None):
         ttk.Label.__init__(self, master, text=text, cursor="hand2", style=style)
 
         if isinstance(to, str):
             self.to = [to]
         else:
-            self.to = to
+            self.to = list(to)
         self.subject = subject
         self.msg = msg
 
         self.bind("<Button-1>", self.mailto)
     
-    def __convert(self, text):
+    def __convert(self, text:str):
         text = re.sub(r" ", "%20", text)
         text = re.sub(r"\n", "%0D", text)
         text = re.sub(r"<", "%3C", text)
